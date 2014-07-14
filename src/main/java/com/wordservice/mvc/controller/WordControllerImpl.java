a097de24f93b2b930954de289f1697b4c61cc025
ilja <ilja.guzovski@gmail.com>
2014-07-14 14:24:27 +0300

    iterration??

--- src/main/java/com/wordservice/mvc/controller/WordControllerImpl.java
+++ src/main/java/com/wordservice/mvc/controller/WordControllerImpl.java
@@ -3,6 +3,7 @@ package com.wordservice.mvc.controller;
 import com.wordservice.mvc.model.WordEntity;
 import com.wordservice.mvc.repository.WordRepository;
 import com.wordservice.mvc.repository.WordRepositoryFixedIndexesSearch;
+import com.wordservice.mvc.service.wordfinder.WordTupleFinderService;
 import com.wordservice.mvc.service.wordsaver.TextSaverService;
 import com.wordservice.mvc.util.WordPopularityComparator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,9 @@ public class WordControllerImpl {
     @Autowired
     private TextSaverService textSaverService;
 
+    @Autowired
+    private WordTupleFinderService wordTupleFinderService;
+
 
     @RequestMapping(value = "getTopFor/{word}", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
@@ -35,6 +39,19 @@ public class WordControllerImpl {
     @ResponseBody
     public List<WordEntity> get10TopWordsAfter(@PathVariable String previous, @PathVariable String last) {
         return wordRepository.getTop10WordsAfter(clean(previous), clean(last));
+    }
+
+
+    @RequestMapping(value = "context/{f}/{s}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public List<WordEntity> getByFirstTwo(@PathVariable String f, @PathVariable String s) {
+        return wordRepository.getTop10WordsAfter(clean(f), clean(s));
+    }
+
+    @RequestMapping(value = "context/{f}/{s}/{t}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public List<WordEntity> getByFirstTwo(@PathVariable String f, @PathVariable String s, @PathVariable String t) {
+        return wordTupleFinderService.getNextWords(clean(f), clean(s), clean(t));
     }
 
 
