7292c359c914afbfe835af9a5f56d28016e85cc2
ilja <ilja.guzovski@gmail.com>
2014-07-26 11:46:59 +0300

    better finding in repository

--- src/main/java/com/wordservice/mvc/repository/WordRepository.java
+++ src/main/java/com/wordservice/mvc/repository/WordRepository.java
@@ -17,7 +17,7 @@ public interface WordRepository extends GraphRepository<WordEntity> {
 
     @Query("START wordEntity=node:word(word={word1}), wordEntity2=node:word(word={word2}) " +
             "MATCH wordEntity-[r:IS_FOLLOWED_BY]->wordEntity2-[r2:IS_FOLLOWED_BY]->otherWord" +
-            " RETURN otherWord ORDER BY r2.popularity DESC LIMIT 10")
+            " RETURN otherWord ORDER BY r.popularity, r2.popularity DESC LIMIT 10")
     List<WordEntity> getTop10WordsAfter(@Param("word1") String word1, @Param("word2") String word2);
 
     @Query("START wordEntity= node(*) WHERE has(wordEntity.word) AND wordEntity.word =~ {word1}" +
